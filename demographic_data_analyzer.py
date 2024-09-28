6f06f5faf7ed18cf82e093185722f0d6063a652c
odanielaugusto <158776621+odanielaugusto@users.noreply.github.com>
2024-09-27 21:04:58 -0300

    feat: demographic_data_analyzer

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -2,45 +2,50 @@ import pandas as pd
 
 
 def calculate_demographic_data(print_data=True):
-    # Read data from file
-    df = None
+    #Read data from file
+    df = pd.read_csv("boilerplate-demographic-data-analyzer/adult.data.csv")
 
-    # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    #How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
+    race_count = df['race'].value_counts()
 
-    # What is the average age of men?
-    average_age_men = None
+    #What is the average age of men?
+    average_age_men = round(df[df['sex'] == 'Male']['age'].mean(), 1)
 
-    # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    #What is the percentage of people who have a Bachelor's degree?
+    percentage_bachelors = round((df[df['education'] == 'Bachelors'].shape[0] / df.shape[0]) * 100, 1)
 
-    # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
-    # What percentage of people without advanced education make more than 50K?
+    #What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
+    #What percentage of people without advanced education make more than 50K?
 
-    # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    #with and without `Bachelors`, `Masters`, or `Doctorate`
+    higher_education = df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])
+    lower_education = df[~df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
 
-    # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    #percentage with salary >50K
+    higher_education_rich = round((df[higher_education & (df['salary'] == '>50K')].shape[0] / df[higher_education].shape[0]) * 100, 1)
+    lower_education_rich = round((df[~higher_education & (df['salary'] == '>50K')].shape[0] / df[~higher_education].shape[0]) * 100, 1)
 
-    # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    #What is the minimum number of hours a person works per week (hours-per-week feature)?
+    min_work_hours = df['hours-per-week'].min()
 
-    # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    #What percentage of the people who work the minimum number of hours per week have a salary of >50K?
+    num_min_workers = df[df['hours-per-week'] == min_work_hours]
+    rich_percentage = (num_min_workers[num_min_workers['salary'] == '>50K'].shape[0] / num_min_workers.shape[0]) * 100
 
-    rich_percentage = None
+    #What country has the highest percentage of people that earn >50K?
+    country_rich_counts = df[df['salary'] == '>50K']['native-country'].value_counts()
 
-    # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = None
-    highest_earning_country_percentage = None
+    total_country_counts = df['native-country'].value_counts()
 
-    # Identify the most popular occupation for those who earn >50K in India.
-    top_IN_occupation = None
+    rich_percentage_by_country = (country_rich_counts / total_country_counts) * 100
 
-    # DO NOT MODIFY BELOW THIS LINE
+    highest_earning_country_percentage = round(rich_percentage_by_country.max(), 1)
+    highest_earning_country = rich_percentage_by_country.idxmax()
+
+    #Identify the most popular occupation for those who earn >50K in India.
+    top_IN_occupation = df[(df['native-country'] == 'India') & (df['salary'] == '>50K')]['occupation'].value_counts().idxmax()
+
+    #DO NOT MODIFY BELOW THIS LINE
 
     if print_data:
         print("Number of each race:\n", race_count) 
